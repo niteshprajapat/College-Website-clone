0b49c1246b30189a042f4d2bea484d240a8dbdc4
Nitesh Prajapat <nitesh.19jdml019@jietjodhpur.ac.in>
2021-12-28 23:25:01 +0530

    JS added for hamburger effect

---
+++ app.js
@@ -0,0 +1,11 @@
+// JS for hamburger
+
+let navLinks = document.getElementById("navLinks")
+
+const showMenu = ()=>{
+    navLinks.style.right = "0";
+}
+
+const hideMenu = ()=>{
+    navLinks.style.right = "-320px";
+}
